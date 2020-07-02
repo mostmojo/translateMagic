6a2c56ef4ef5d2709d0ae08e32fd135c3bcf780f
Jacob Papageorgiou <jacob.papageorgiou@gmail.com>
2020-07-02 14:28:07 +0300

    Add flags

---
+++ app.js
@@ -0,0 +1,30 @@
+var linguJSON = {
+	languages: [{
+			lang_name: 'English',
+			lang_code: 'en',
+			url_pattern: '?',
+		},
+		{
+			lang_name: 'Español',
+			lang_code: 'es',
+			url_pattern: '?',
+		},
+	],
+	translated_segments: [{
+			source: 'V1: Ready to translate this?',
+			target_es: 'V1:¿Está listo para el mundo?',
+		},
+		{
+			source: 'Get found by search engines and visitors in other languages!',
+			target_es: 'Sea encontrado por los motores de búsqueda en otros idiomas',
+		},
+		{
+			source: 'Go to the top right corner & change language. Watch the magic happen! 🤓',
+			target_es: 'Ve a la esquina superior derecha y cambia el idioma. ¡Mira cómo sucede la magia! 🤓',
+		},
+		{
+			source: 'Powered by Empire Media',
+			target_es: 'Potenciado por Empire Media',
+		},
+	],
+};
